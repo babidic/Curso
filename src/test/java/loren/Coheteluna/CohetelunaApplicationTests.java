33d29df99a0f5f1964a4328948147808571e0fca
babidic <31072246+babidic@users.noreply.github.com>
2024-06-05 09:58:53 +0200

    Add files via upload

---
+++ src/test/java/loren/Coheteluna/CohetelunaApplicationTests.java
@@ -0,0 +1,13 @@
+package loren.Coheteluna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohetelunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
